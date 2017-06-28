2643341f9079beb6b63047315e20c59691866206
Steven Cheng <stevenc81@gmail.com>
2017-06-27 17:38:17 -0700

    Add lib code

---
+++ src/com/buffer/Pipeline.groovy
@@ -0,0 +1,146 @@
+#!/usr/bin/groovy
+
+package com.buffer;
+
+def gitEnvVars() {
+    // create git envvars
+    println "Setting envvars to tag container"
+
+    sh 'git rev-parse HEAD > git_commit_id.txt'
+    try {
+        env.GIT_COMMIT_ID = readFile('git_commit_id.txt').trim()
+        env.GIT_SHA = env.GIT_COMMIT_ID.substring(0, 7)
+    } catch (e) {
+        error "${e}"
+    }
+    println "env.GIT_COMMIT_ID ==> ${env.GIT_COMMIT_ID}"
+
+    sh 'git config --get remote.origin.url> git_remote_origin_url.txt'
+    try {
+        env.GIT_REMOTE_URL = readFile('git_remote_origin_url.txt').trim()
+    } catch (e) {
+        error "${e}"
+    }
+    println "env.GIT_REMOTE_URL ==> ${env.GIT_REMOTE_URL}"
+}
+
+def kubectlTest() {
+    // Test that kubectl can correctly communication with the Kubernetes API
+    println "checking kubectl connnectivity to the API"
+    sh "kubectl get nodes"
+
+}
+
+def helmConfig() {
+    //setup helm connectivity to Kubernetes API and Tiller
+    println "initiliazing helm client"
+    sh "helm init"
+    println "checking client/server version"
+    sh "helm version"
+}
+
+def getContainerTags(config, Map tags = [:]) {
+
+    println "getting list of tags for container"
+    def String commit_tag
+    def String version_tag
+
+    try {
+        // if PR branch tag with only branch name
+        if (env.BRANCH_NAME.contains('PR')) {
+            commit_tag = env.BRANCH_NAME
+            tags << ['commit': commit_tag]
+            return tags
+        }
+    } catch (Exception e) {
+        println "WARNING: commit unavailable from env. ${e}"
+    }
+
+    // commit tag
+    try {
+        // if branch available, use as prefix, otherwise only commit hash
+        if (env.BRANCH_NAME) {
+            commit_tag = env.BRANCH_NAME + '-' + env.GIT_COMMIT_ID.substring(0, 7)
+        } else {
+            commit_tag = env.GIT_COMMIT_ID.substring(0, 7)
+        }
+        tags << ['commit': commit_tag]
+    } catch (Exception e) {
+        println "WARNING: commit unavailable from env. ${e}"
+    }
+
+    // master tag
+    try {
+        if (env.BRANCH_NAME == 'master') {
+            tags << ['master': 'latest']
+        }
+    } catch (Exception e) {
+        println "WARNING: branch unavailable from env. ${e}"
+    }
+
+    // build tag only if none of the above are available
+    if (!tags) {
+        try {
+            tags << ['build': env.BUILD_TAG]
+        } catch (Exception e) {
+            println "WARNING: build tag unavailable from config.project. ${e}"
+        }
+    }
+
+    return tags
+}
+
+@NonCPS
+def getMapValues(Map map=[:]) {
+    // jenkins and workflow restriction force this function instead of map.values(): https://issues.jenkins-ci.org/browse/JENKINS-27421
+    def entries = []
+    def map_values = []
+
+    entries.addAll(map.entrySet())
+
+    for (int i=0; i < entries.size(); i++){
+        String value =  entries.get(i).value
+        map_values.add(value)
+    }
+
+    return map_values
+}
+
+def helmLint(String chart_dir) {
+    // lint helm chart
+    println "running helm lint ${chart_dir}"
+    sh "helm lint ${chart_dir}"
+
+}
+
+def helmDeploy(Map args) {
+    //configure helm client and confirm tiller process is installed
+    helmConfig()
+
+    if (args.dry_run) {
+        println "Running dry-run deployment"
+
+        sh "helm upgrade --dry-run --install ${args.name} ${args.chart_dir} --set image.tag=${args.version_tag} --namespace=${args.namespace}"
+    } else {
+        println "Running deployment"
+        sh "helm upgrade --install --wait ${args.name} ${args.chart_dir} --set image.tag=${args.version_tag} --namespace=${args.namespace}"
+
+        echo "Application ${args.name} successfully deployed. Use helm status ${args.name} to check"
+    }
+}
+
+def helmTest(Map args) {
+    println "Running Helm test"
+
+    sh "helm test ${args.name} --cleanup"
+}
+
+def containerBuildPub(Map args) {
+
+    println "Running Docker build/publish: ${args.host}/${args.acct}/${args.repo}:${args.tags} with credentials in Jenkins ${args.auth_id}"
+
+    for (int i = 0; i < args.tags.size(); i++) {
+      sh "docker build -t ${args.acct}/${args.repo}:${args.tags[i]} ./"
+      sh "docker push ${args.acct}/${args.repo}:${args.tags[i]}"
+    }
+}
