654f1ace9bbc15c709b2ca48bea339af97ad9d9b
Steven Cheng <stevenc81@gmail.com>
2017-10-12 09:22:03 +0800

    Update the version for kubectl and helm

--- src/com/buffer/Pipeline_marketing.groovy
+++ src/com/buffer/Pipeline_marketing.groovy
@@ -197,8 +197,8 @@ def start(String configFile) {
 
     podTemplate(label: 'pipeline-pod', containers: [
         containerTemplate(name: 'docker', image: 'docker:17.06.0', ttyEnabled: true, command: 'cat'),
-        containerTemplate(name: 'helm', image: 'lachlanevenson/k8s-helm:v2.4.2', ttyEnabled: true, command: 'cat'),
-        containerTemplate(name: 'kubectl', image: 'lachlanevenson/k8s-kubectl:v1.6.6', ttyEnabled: true, command: 'cat'),
+        containerTemplate(name: 'helm', image: 'lachlanevenson/k8s-helm:v2.6.2', ttyEnabled: true, command: 'cat'),
+        containerTemplate(name: 'kubectl', image: 'lachlanevenson/k8s-kubectl:v1.8.0', ttyEnabled: true, command: 'cat'),
         containerTemplate(name: 'node', image: 'node:8.1.3', ttyEnabled: true, command: 'cat')
     ],
     volumes:[
